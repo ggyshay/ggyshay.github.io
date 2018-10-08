9388494dbca10c77259e7d5ddf1eca691a439d69
Gabriel Yshay <ggyshay@Gabriels-MacBook-Pro.local>
2018-10-08 19:56:45 -0300

    importing files

---
+++ precache-manifest.a406ce5dce961f340123079179b4bc36.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f07b209c33dba84caaaa059152df88c4",
+    "url": "/static/media/profile.f07b209c.jpg"
+  },
+  {
+    "revision": "4e2ba1f2f6678a57d218795698056e48",
+    "url": "/static/media/Group 4.4e2ba1f2.png"
+  },
+  {
+    "revision": "3a5e972c5b2364536df856fd6c2357e5",
+    "url": "/static/media/Group 3.3a5e972c.png"
+  },
+  {
+    "revision": "59229f42c54270c9b00ce205d1a8c49a",
+    "url": "/static/media/Group 2.59229f42.png"
+  },
+  {
+    "revision": "85866f2513a940851dd12b7da25708b1",
+    "url": "/static/media/Group 1.85866f25.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "593e0a9fda7fd16d5cb9",
+    "url": "/static/js/main.593e0a9f.chunk.js"
+  },
+  {
+    "revision": "bed89ffe4cc7a2818f1b",
+    "url": "/static/js/1.bed89ffe.chunk.js"
+  },
+  {
+    "revision": "593e0a9fda7fd16d5cb9",
+    "url": "/static/css/main.e038c093.chunk.css"
+  },
+  {
+    "revision": "2ed9b092c0dea5d59b0e8ee0b2fb4d76",
+    "url": "/index.html"
+  }
+];
