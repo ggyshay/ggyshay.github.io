eb43c254a2a1afd3353b6bf1ada8ea4ca7dababa
yshay <yshay@cuidas.com.br>
2020-09-07 19:43:35 -0300

    add new proj

---
+++ static/precache-manifest.c40ec3540b3b889e215efc310a071b92.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cea491abd9be58e8af07",
+    "url": "/static/css/main.d1670830.chunk.css"
+  },
+  {
+    "revision": "cea491abd9be58e8af07",
+    "url": "/static/js/main.cea491ab.chunk.js"
+  },
+  {
+    "revision": "b5d28093fc0ae105e716",
+    "url": "/static/js/1.b5d28093.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7baa3e2c179dadae86faad05fb9905d7",
+    "url": "/static/media/Group 3.7baa3e2c.png"
+  },
+  {
+    "revision": "f7ac41d012c21a08f7012d6dbee7a458",
+    "url": "/static/media/Group 4.f7ac41d0.png"
+  },
+  {
+    "revision": "8cd3da4b97e3db294ada2d5438e477d5",
+    "url": "/static/media/Group 2.8cd3da4b.png"
+  },
+  {
+    "revision": "9938b741ba5686208ef27eadc3c13ce2",
+    "url": "/static/media/profile.9938b741.jpg"
+  },
+  {
+    "revision": "85866f2513a940851dd12b7da25708b1",
+    "url": "/static/media/Group 1.85866f25.png"
+  },
+  {
+    "revision": "bedf8339ffbe077498940e73287e1ef8",
+    "url": "/static/media/stock_model.bedf8339.png"
+  },
+  {
+    "revision": "d1ab540b61ebce69b68870ef36beb56b",
+    "url": "/index.html"
+  }
+];
